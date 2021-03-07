ffb261f6b5c520eaba1d7b1b8c313bccd3f82452
Tarana <75437852+taranahassan@users.noreply.github.com>
2021-03-07 17:47:51 -0500

    Importing data.js into script to create table with data and filters.

--- app.js
+++ app.js
@@ -18,6 +18,33 @@ data.forEach((dataRow) => {
     Object.values(dataRow).forEach((val) => {
         let cell = row.append('td');
         cell.text(val);
-     }
+    }
     );
 });
+
+function handleClick() {
+    // grabbing datetime value from the filter
+    let date = d3.select('#datetime').property('value');
+    let filteredData = tableData;
+    // check to see if date was entered and filter the date using that date
+    if (date) {
+        // apply 'filter' to table data to only keep rows where the 
+        // 'datetime' value matches filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+// rebuilding table using filtered data
+// @NOTE: if no date was entered, then filteredData will
+// just be the original tableData
+buildTable(filteredData);
+};
+
+// creating filter-btn id to link to button tag in html
+//(attaching event to listen for form button)
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+// building table with original data when page loads
+buildTable(tableData);
+
+
+
+
